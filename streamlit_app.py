48ff51d90df353cbeb8e98fe4bd9aa24a02117bc
annikaijak <143806022+annikaijak@users.noreply.github.com>
2023-12-04 17:08:56 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -176,16 +176,7 @@ with tab3:
   st.header('Transformer Approach')
   st.write('This tab includes Transformer-Based Sentiment Analysis using RoBERTa and SoftMax.')
 
-  with st.form('my_form3'):
-    st.subheader('Sentiment Analysis for Individual Reviews')
 
-    review_txt3 = st.text_input('Enter your review here')
-      
-    submit_button3 = st.form_submit_button('Submit')
-      
-    if submit_button3:
-      result2 = predict_sentiment(review_txt3)
-      st.write(result2)
 
 
 with tab4:
