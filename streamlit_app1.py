5ed10ef3a8416a0eb153eea9a7cfe5e9ffc40ac5
annikaijak <143806022+annikaijak@users.noreply.github.com>
2023-12-05 12:48:41 +0100

    Update streamlit_app1.py

--- streamlit_app1.py
+++ streamlit_app1.py
@@ -174,6 +174,17 @@ with tab3:
   
       return f"This review has {sentiment} sentiment with a score of {scores[sentiment]*100:.2f}%"
 
+  with st.form('my_form3'):
+    st.subheader('Sentiment Analysis for Individual Reviews')
+
+    review_txt3 = st.text_input('Enter your review here')
+      
+    submit_button3 = st.form_submit_button('Submit')
+      
+    if submit_button3:
+      result2 = predict_sentiment(review_txt3)
+      st.write(result2)
+
 with tab4:
   st.header('Model performance')
 
